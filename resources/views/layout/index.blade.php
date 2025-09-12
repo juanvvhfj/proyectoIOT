e25b907c09dde51007848058bb11cc44f888918d
juan <fernando1990788@gmail.com>
2025-09-11 19:39:38 -0500

    trabajo de iot

---
+++ resources/views/layout/index.blade.php
@@ -0,0 +1,15 @@
+
+@extends('layout.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="card" style="width: 18rem;">
+        <img src="https://d2xqcz296oofyv.cloudfront.net/wp-content/uploads/2019/07/IoT.jpg" alt="IoT Image">
+        <div class="card-body">
+            <h5 class="card-title">Card Title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+@endsection
